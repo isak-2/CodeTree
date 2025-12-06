4220abe754020483f3b348af3ad4a0dac5ae099f
isak-2 <kadasian21@gmail.com>
2025-12-07 07:39:43 +0900

    Title: 약수가 세 개인 수, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251207/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 251207/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -2,11 +2,11 @@ start, end = map(int, input().split())
 
 # Please write your code here.
 total_count = 0
-for i in range(start,end+1):
+for i in range(start,end+1,1):
     divisor_count = 0
-    for j in range(1,i+1):
+    for j in range(1,i+1,1):
         if i % j == 0:
             divisor_count += 1
     if divisor_count == 3:
-        total_count =+ 1
+        total_count += 1
 print(total_count)
