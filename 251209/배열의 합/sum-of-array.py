a12f524edb64a23f12fb4818f147bbb2b221e324
isak-2 <kadasian21@gmail.com>
2025-12-09 20:31:05 +0900

    Title: 배열의 합, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251209/배열의 합/sum-of-array.py
@@ -0,0 +1,8 @@
+lst1 = [int(x) for x in input().split()]
+lst2 = [int(x) for x in input().split()]
+lst3 = [int(x) for x in input().split()]
+lst4 = [int(x) for x in input().split()]
+print(sum(lst1))
+print(sum(lst2))
+print(sum(lst3))
+print(sum(lst4))
