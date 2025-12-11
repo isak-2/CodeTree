853d4b193c579efd0e56567a0dc9d03a8469ca3e
isak-2 <kadasian21@gmail.com>
2025-12-11 22:14:52 +0900

    Title: 상하로 반복하며 출력, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251211/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,21 @@
+N = int(input())
+arr = [[0 for i in range(N)] for i in range(N)]
+
+for i in range(N):
+    cnt = 1
+    if i % 2 == 0: 
+        for j in range(N):
+            arr[j][i] = cnt
+            cnt += 1 
+    else:
+        for j in range(N-1,-1,-1):
+            arr[j][i] = cnt
+            cnt += 1 
+    cnt = 0 
+
+
+
+for i in arr:
+    for j in i:
+        print(j, end='')
+    print( )
