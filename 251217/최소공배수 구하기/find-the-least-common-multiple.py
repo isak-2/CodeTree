12b1ba2612884d9e1bf28434bea20a98287716f7
isak-2 <kadasian21@gmail.com>
2025-12-17 21:32:56 +0900

    Title: 최소공배수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251217/최소공배수 구하기/find-the-least-common-multiple.py
+++ 251217/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,20 +1,12 @@
 n, m = map(int, input().split())
 
-# Please write your code here.
-m,n  = max(m,n), min(m,n)
-if m % n == 0:
-    lcm = m 
-else: 
-    for i in range(1,n+1):
-        if m % i == 0 and n % i == 0:
-            gcd = i
 
-    
-    for i in range(1,m):
-        if m == gcd * i:
-            lcm_s1 = i 
-        if n == gcd * i:
-            lcm_s2 = i
-    lcm = gcd * lcm_s1 * lcm_s2 
+# 최소 공배수를 구하고 
+for i in range(1, min(m,n)+1):
+    if m % i == 0 and n % i == 0:
+        gcd = i 
+
+# 점화식을 쓴다. 
+lcm = (m * n) // gcd 
 
 print(lcm)
