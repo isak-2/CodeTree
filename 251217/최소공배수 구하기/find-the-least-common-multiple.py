94364da41348ae69ab658505125ffd3323a29c2f
isak-2 <kadasian21@gmail.com>
2025-12-17 21:15:26 +0900

    Title: 최소공배수 구하기, Time: 87ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251217/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+m,n  = max(m,n), min(m,n)
+if m % n == 0:
+    lcm = m 
+else: 
+    for i in range(1,n+1):
+        if m % i == 0 and n % i == 0:
+            gcd = i
+
+    
+    for i in range(1,gcd):
+        if m == gcd * i:
+            lcm_s1 = i 
+        if n == gcd * i:
+            lcm_s2 = i
+    lcm = gcd * lcm_s1 * lcm_s2 
+
+print(lcm)
