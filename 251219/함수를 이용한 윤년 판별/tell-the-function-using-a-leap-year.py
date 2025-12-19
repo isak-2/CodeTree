c215cbcba472782c10268bf6906ccf743c7f769a
isak-2 <kadasian21@gmail.com>
2025-12-19 19:56:20 +0900

    Title: 함수를 이용한 윤년 판별, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251219/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,13 @@
+y = int(input())
+
+# Please write your code here.
+def func(y):
+    if y % 4 == 0:
+        if y % 100 == 0 and y % 400 != 0:
+            return 'false'
+        return 'true'
+    else:
+        return 'false'
+
+
+print(func(y))
