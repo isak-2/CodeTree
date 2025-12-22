fa5630db8eec499e0ae951ca1b5ea4b8a3cf469a
isak-2 <kadasian21@gmail.com>
2025-12-22 22:56:00 +0900

    Title: 2021년 날짜의 유무, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251222/2021년 날짜의 유무/with-or-without-2021.py
+++ 251222/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,6 +1,7 @@
 M, D = map(int, input().split())
 
-
+month = 12 
+m_result = month - M 
 if M in [1,3,5,7,8,10,12]: #31일까지 있는 달 
     day1 = 31
     result = day1 - D 
@@ -12,7 +13,7 @@ else:  #28일까지 있는 달
     result = day3 - D     
 
 # Please write your code here.
-if result >= 0:
+if result >= 0 and m_result >= 0:
     print('Yes')
 else:
     print('No')
