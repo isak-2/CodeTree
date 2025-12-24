bb40c5569f7be53c19527c4f9a137b7e61b75dd6
isak-2 <kadasian21@gmail.com>
2025-12-24 21:50:23 +0900

    Title: 두 정수에 대한 연산값, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251224/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 251224/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -3,18 +3,16 @@ a, b = map(int, input().split())
 # Please write your code here.
 def swap(a,b):
     if a > b:
-        big = a + 25
-        small = b * 2
+        a = a + 25
+        b = b * 2
     else:
-        big = b + 25
-        small = a * 2
+        b = b + 25
+        a = a * 2
     
-    if big < small:
-        big, small = small, big 
 
-    return small, big
+    return a,b
 
-small, big = swap(a,b)
+a,b = swap(a,b)
 
-print(small, big)
+print(a,b)
 
