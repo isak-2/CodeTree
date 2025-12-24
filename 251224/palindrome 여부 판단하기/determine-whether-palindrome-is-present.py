017d252353d385d6167501e8898e366216f5f106
isak-2 <kadasian21@gmail.com>
2025-12-24 21:23:15 +0900

    Title: palindrome 여부 판단하기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251224/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 251224/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,14 +1,5 @@
 A = input()
-
-# Please write your code here.
-def palindrome(sentence):
-    B=""
-    length = len(sentence)
-    for i in range(length-1,-1,-1):
-        B += sentence[i]
-    return B
-
-B = palindrome(A)
+B = A[::-1]
 
 if A == B:
     print('Yes')
